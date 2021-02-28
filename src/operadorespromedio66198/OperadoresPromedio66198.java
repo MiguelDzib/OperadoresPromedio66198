f3e40c9c94f27c1df78d15b789680b2d59b0d5f2
galbe <galbe@192.168.1.68>
2021-02-28 09:58:32 -0600

    Operadores promedio 66198

---
+++ src/operadorespromedio66198/OperadoresPromedio66198.java
@@ -0,0 +1,19 @@
+
+package operadorespromedio66198;
+
+public class OperadoresPromedio66198 {
+
+    public static void main(String[] args) {
+        double Calificacion1=5 ;
+        double Calificacion2=4.3 ; 
+        double Calificacion3=3;
+        double Calificacion4=10 ;
+        double Calificacion5=7 ;
+        double  Promedio ;
+        
+        Promedio = (Calificacion1 + Calificacion2 + Calificacion3 + Calificacion4 + Calificacion5 )/5;
+        
+        System.out.println (" Tu promedio sera " + Promedio) ;
+    }
+    
+}
